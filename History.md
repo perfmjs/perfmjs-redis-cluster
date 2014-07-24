c254e9846fd03313de37447f12283b1e369c935c
perfmjs <19589425@qq,cin>
2014-07-24 21:57:58 +0800

    1.0.0 / 2014-07-23 ==================  * the first stable version

--- History.md
+++ History.md
@@ -1,3 +1,12 @@
+1.0.0 / 2014-07-23
+==================
+ * the first stable version
+
+
+0.0.2 / 2014-07-23
+==================
+ * fix: add return at init function
+
 0.0.1 / 2014-07-23
 ==================
  * Init: init version
---
+++ test/perfmjs-spec.js
@@ -0,0 +1,28 @@
+/**
+ * npm install jasmine-node -g
+ * 在项目根路径下运行测试的方法： jasmine-node test/
+ * 参考：https://www.npmjs.org/package/jasmine-node
+ * Created by tony on 2014/7/24
+ */
+describe("测试perfmjs-redis-cluster", function () {
+    beforeEach(function() {
+        require("perfmjs-node");
+    });
+    it("应能测试通过redis#set方法", function() {
+        require('../lib');
+        perfmjs.ready(function($$, app) {
+            app.register('redisCluster', $$.redisCluster);
+            app.start('redisCluster');
+            var redisHost = "localhost";
+            var startNodes = [{host:redisHost, port:7001}, {host:redisHost, port:7002}, {host:redisHost, port:7003}];
+            var redisCluster = $$.redisCluster.instance.initStartupOptions(startNodes);
+            redisCluster.set('foo', 'test123', function(err, reply) {
+                redisCluster.get('foo', function (err, reply) {
+                    expect(1).toEqual(2);
+                    console.log('reply:' + reply);
+                    redisCluster.end();
+                });
+            });
+        });
+    });
+});
